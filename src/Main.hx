7cfae1b37fc7f16fb30732613033778a3ce12a73
Bioruebe <william@nature-space.de>
2017-04-18 05:18:00 +0200

    Fixed error message/cards HTML file not opening on Windows 10 Creator's Update  Added showcase link  Changed dependency from curl to akifox-asynchttp  Rewrote request logic

--- src/Main.hx
+++ src/Main.hx
@@ -16,15 +16,16 @@
 
 package ;
 
-import haxe.Curl;
+//import haxe.Curl;
+import com.akifox.asynchttp.HttpHeaders;
+import com.akifox.asynchttp.HttpRequest;
+import com.akifox.asynchttp.HttpResponse;
 import haxe.ds.StringMap;
 import haxe.Http;
 import haxe.Json;
 import haxe.Serializer;
 import haxe.Timer;
 import haxe.Unserializer;
-import neko.Lib;
-import neko.vm.Thread;
 import sys.io.File;
 import sys.io.Process;
 
@@ -47,33 +48,16 @@ class Main
 	static var gameNames:StringMap<String>;
 	static var cookie = "";
 	
-	static function main() 
-	{
+	static function main() {
 		var timestamp:Float = Timer.stamp();
-		Bio.Header("Bioruebe's Trading Card Bot Availability Checker", "1.04", "A simple tool to compare a Steam Inventory with available cards at http://www.steamcardexchange.net/index.php?inventory", "[-n:name]\n	-n		Use 'name' as steamID instead of reading it from file\n\n!Cards already in the inventory are skipped.\n!Only sets of which at least one card is owned are considered.\n!If a complete set is in the inventory, no cards will be searched. Craft a badge, then try again.\n");
+		Bio.Header("Bioruebe's Trading Card Bot Availability Checker", "1.10", "A simple tool to compare a Steam Inventory with available cards at http://www.steamcardexchange.net/index.php?inventory", "[-n:name]\n	-n		Use 'name' as steamID instead of reading it from file\n\n!Cards already in the inventory are skipped.\n!Only sets of which at least one card is owned are considered.\n!If a complete set is in the inventory, no cards will be searched. Craft a badge, then try again.\n");
 		
-		getCookie();
-		
-		trace(cookie);
-		
-		//Sys.exit(0);
-		
-		// Load appID data
-		//try {
-			//var serializedString = File.getContent(sBasePath + "appIDs.db");
-			//gameNames = Unserializer.run(serializedString);
-			////trace("unserialised");
-		//}
-		//catch (err:Dynamic){
-			gameNames = new StringMap();
-		//}
+		gameNames = new StringMap();
 		
 		var arg:String = Sys.args().pop();
 		if (arg != null) {
 			var pos:Int = arg.indexOf("-n:");
-			if (pos != -1) {
-				sSteamID = arg.substr(pos + 3);
-			}
+			if (pos != -1) sSteamID = arg.substr(pos + 3);
 		}		
 		
 		// Get SteamID
@@ -89,13 +73,14 @@ class Main
 			Sys.exit(1);
 		}
 		
+		getCookie();
+		trace(cookie);
+		
 		Request("http://steamcommunity.com/id/" + sSteamID + "/inventory/json/753/6", OnSteamCards);
 		
 		var message:HttpRequest;
 		while (iRequests > 0) {
-			message = Thread.readMessage(true);
-			message.dataHandler(message);
-			iRequests--;
+			Sys.sleep(1);
 		}
 		
 		//File.saveContent(sBasePath + "appIDs.db", Serializer.run(gameNames));
@@ -103,13 +88,13 @@ class Main
 		trace("\n\n\n\n\n");
 		
 		// Print results and create HTML file
-		var html:String = "<h2><a href=\"http://steamcommunity.com/tradeoffer/new/?partner=83905207&token=tEx7-bXd\">Make Offer</a></h2><h3><a href= \"http://www.steamcardexchange.net/index.php?profile\">Your Credits</a></h3>";
-		var gameName:String;
+		var html = "<h2><a href=\"http://steamcommunity.com/tradeoffer/new/?partner=83905207&token=tEx7-bXd\">Make Offer</a></h2><h3><a href= \"http://www.steamcardexchange.net/index.php?profile\">Your Credits</a></h3>";
+		var gameName;
 		for (o in cardOffers) {
 			gameName = gameNames.get(o.appID);
 			if (html.indexOf(gameName) == -1) {
 				if (html.indexOf("<ul>") != -1) html += "</ul>";
-				html += "<br><b><a href=\"http://steamcommunity.com/id/" + sSteamID + "/gamecards/" + o.appID + "\">" + gameName + "</a></b><br><ul>";
+				html += "<br><b><a href=\"http://steamcommunity.com/id/" + sSteamID + "/gamecards/" + o.appID + "\">" + gameName + "</a></b>&nbsp;<small><a href=\"" + StringTools.replace(o.url, "inventorygame", "gamepage") + "\">Showcase</a></small><br><ul>";
 			}
 			html += "<li>" + o.amount + " x <a href=\"" + o.tradeLink + "\">" + o.name + "</a> a <a href=\"" + o.url + "\">" + o.price + " Credits</a>";
 			trace(gameName + ": " + o.amount + " x " + o.name + " a " + o.price + " Credits");
@@ -118,14 +103,19 @@ class Main
 		
 		// Write to HTML file and execute
 		File.saveContent(sBasePath + "Cards.html", html);
-		new Process("cmd", ["/c " + sBasePath + "Cards.html"]);
+		Sys.command("start", [sBasePath + "Cards.html"]);
 		
 		trace("\nCompleted! Processing cards for " + iGameCount + " games took a total of " + Std.string(Std.int(Math.fceil((Timer.stamp() - timestamp) * 100)) / 100) + " seconds.");
 		trace(cardOffers.length + " missing cards available for trade.\n");
 	}
 	
-	static function OnSteamCards(request:HttpRequest) {
-		var data:String = request.response;
+	static function OnSteamCards(response:HttpResponse) {
+		if (!response.isOK) {
+			trace('HTTP error ${response.status} ${response.error})');
+			Sys.exit(2);
+		}
+		
+		var data = response.content;
 		var cardData:Dynamic = null;
 		
 		// Parse JSON
@@ -137,7 +127,6 @@ class Main
 		catch (err:Dynamic) {
 			trace("Error: Invalid JSON data. Aborting.");
 			Sys.exit(1);
-			return;
 		}
 		
 		for (obj in Reflect.fields(cardData.rgDescriptions)) {
@@ -151,7 +140,7 @@ class Main
 			}
 			
 			// Insert appid and array into map
-			var allCards:Array<CardInfo> = cardRequests.get(card.market_fee_app);
+			var allCards = cardRequests.get(card.market_fee_app);
 			if (allCards == null) {
 				var cards:Array<CardInfo> = new Array();
 				cards.push(card);
@@ -162,59 +151,64 @@ class Main
 			}
 			//cards.push(card);
 		}
-		var i:Int = 0;
+		var i = 0;
 		for (key in cardRequests.keys()) {
 			trace("Processing " + key);
 			//for (k in cardRequests.get(key)) {
 				//trace(k.market_fee_app + " - " + k.name);
 			//}
-			
 			//break;
-			Request("http://www.steamcardexchange.net/index.php?inventorygame-appid-" + key, OnTradeOffer, cardRequests.get(key));
+			
+			Request("http://www.steamcardexchange.net/index.php?inventorygame-appid-" + key, function(response:HttpResponse) { OnTradeOffer(response, cardRequests.get(key)); }, {"Cookie": cookie } );
 			Sys.sleep(iBetweenRequests);
 			cardRequests.remove(key);
 			i++;
 			//if (i > 4) break;
 		}
 		//trace(cardRequests);
+		iRequests--;
 	}
 	
-	static function OnTradeOffer(request:HttpRequest) {
-		//trace("OnTradeOffer");
+	static function OnTradeOffer(response:HttpResponse, additionalData:Dynamic) {
+		if (!response.isOK) {
+			trace('HTTP error ${response.status} ${response.error})');
+			iRequests--;
+			return;
+		}
+		
 		iGameCount++;
-		//trace(request.response);
-		var data:String = request.response;
+		var data = response.content;
 		//File.saveContent("test.html", data);
 		
 		// Get name to appid
-		if (!gameNames.exists(request.additionalData[0].market_fee_app)) {
-			gameNames.set(request.additionalData[0].market_fee_app, Bio.StringBetween(request.response, "<h2 class=\"empty\">", "</h2>"));
+		if (!gameNames.exists(additionalData[0].market_fee_app)) {
+			gameNames.set(additionalData[0].market_fee_app, Bio.StringBetween(data, "<h2 class=\"empty\">", "</h2>"));
 		}
 		
-		var available:Array<String> = Bio.StringAllBetween(data, "<div class=\"inventory-game-card-item\">", "</div></div>");
+		var available = Bio.StringAllBetween(data, "<div class=\"inventory-game-card-item\">", "</div></div>");
 		for (html in available) {
 			if ((html.indexOf("card-amount red") != -1) || (html.indexOf("card-amount gray") != -1)) {
 				//trace("Red!");
 				continue;
 			}
 			
-			var name:String = StringTools.htmlUnescape(Bio.StringBetween(html, "<span class=\"card-name gray\">", "</span>"));
+			var name = StringTools.htmlUnescape(Bio.StringBetween(html, "<span class=\"card-name gray\">", "</span>"));
 			if (name == "") continue;
 			
-			if (InArray(request.additionalData, name)) {
+			if (InArray(additionalData, name)) {
 				trace(name + " - Owned");
 				continue;
 			}
 			
-			var amount:Int = Std.parseInt(Bio.StringBetween(html, "Stock:", "</span>"));
-			var price:Int = Std.parseInt(Bio.StringBetween(html, "Price: ", " "));
-			var quickTradeLink:String = Bio.StringBetween(html, "element-button\"><a href=\"", "\"");
+			var amount = Std.parseInt(Bio.StringBetween(html, "Stock:", "</span>"));
+			var price = Std.parseInt(Bio.StringBetween(html, "Price: ", " "));
+			var quickTradeLink = Bio.StringBetween(html, "element-button\"><a href=\"", "\"");
 			
-			cardOffers.push(new CardOffer(name, amount, price, request.url, request.additionalData[0].market_fee_app, quickTradeLink));
-			//trace(amount + " x " + name + " a " + price + " Credits");
+			cardOffers.push(new CardOffer(name, amount, price, response.urlString, additionalData[0].market_fee_app, quickTradeLink));
+			trace(amount + " x " + name + " a " + price + " Credits");
 			//trace(cardOffers.pop());
 		}
-		
+		iRequests--;
 	}
 	
 	static function InArray(array:Array<CardInfo>, name:String) {
@@ -225,35 +219,16 @@ class Main
 		return false;
 	}
 	
-	static function Request(url:String, dataHandler:HttpRequest->Void, ?additionalData:Dynamic):Void {
-		iRequests ++;
+	static function Request(url:String, dataHandler:HttpResponse->Void, ?headers:Dynamic):Void {
+		iRequests++;
 		trace(">Sending request: " + url);
-		//var request:HttpRequest = new HttpRequest(url);
-		////request.addHeader("Accept", "text/html");
-		//request.dataHandler = onData;
-		//request.onStatus = function(code:Int) { trace("Status: " + code); }
-		//request.onError = function (message:String) { trace("Error: " + message); }
 		
-		var t = Thread.create(RequestThread);
-		t.sendMessage(Thread.current());
-		t.sendMessage(new HttpRequest(url, dataHandler, additionalData));
-	}
-	
-	static function RequestThread() {
-		var main:Thread = Thread.readMessage(true);
-		var request:HttpRequest = Thread.readMessage(true);
-		
-		request.response = Curl.get(request.url, null, ["Cookie:" + cookie]);
-		var name = request.url.substr(request.url.lastIndexOf("/") + 1);
-		if (name.indexOf("?") != -1) name = name.substr(name.lastIndexOf("?") + 1);
-		//File.saveContent(name + ".html", request.response);
-		
-		main.sendMessage(request);
+		var r = new HttpRequest( { url: url, callback: dataHandler, headers: new HttpHeaders(headers)} );
+		r.send();
 	}
 	
 	// Retrieve session id, without the server will not send data
 	static function getCookie() {
-		// cURL for haxe does not include all features, so a normal http request has to be used
 		trace("Waiting for session ID");
 		var request = new Http("http://www.steamcardexchange.net");
 		request.request(false);
